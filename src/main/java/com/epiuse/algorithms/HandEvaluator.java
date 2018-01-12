dd3c4022ef38d66722209156d90df23d283f6a4c
avanwyk <andrich.van.wyk@advance.io>
2018-01-12 18:00:30 +0200

    Add interface for HandEvaluator
    Implementation TBD.

---
+++ src/main/java/com/epiuse/algorithms/HandEvaluator.java
@@ -0,0 +1,38 @@
+package com.epiuse.algorithms;
+
+import com.epiuse.cards.Hand;
+
+public final class HandEvaluator {
+
+    public boolean hasOnePair(Hand hand) {
+        return false;
+    }
+
+    public boolean hasTwoPair(Hand hand) {
+        return false;
+    }
+
+    public boolean hasThreeOfAKind(Hand hand) {
+        return false;
+    }
+
+    public boolean hasStraight(Hand hand) {
+        return false;
+    }
+
+    public boolean hasFlush(Hand hand) {
+        return false;
+    }
+
+    public boolean hasFullHouse(Hand hand) {
+        return false;
+    }
+
+    public boolean hasFourOfAKind(Hand hand) {
+        return false;
+    }
+
+    public boolean hasStraightFlush(Hand hand) {
+        return false;
+    }
+}
