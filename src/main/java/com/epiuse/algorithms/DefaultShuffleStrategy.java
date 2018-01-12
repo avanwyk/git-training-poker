eb3a5609254f4517c59aa3dc068b57e47a9700e9
avanwyk <andrich.van.wyk@advance.io>
2018-01-12 17:17:45 +0200

    Implement DefaultShuffleStrategy
    Use Fisher Yates shuffle algorithm.

--- src/main/java/com/epiuse/algorithms/DefaultShuffleStrategy.java
+++ src/main/java/com/epiuse/algorithms/DefaultShuffleStrategy.java
@@ -1,13 +1,24 @@
 package com.epiuse.algorithms;
 
+import com.epiuse.cards.Card;
 import com.epiuse.cards.Deck;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.Random;
 
 public final class DefaultShuffleStrategy
-    implements ShuffleStrategy {
+        implements ShuffleStrategy {
 
     @Override
     public Deck shuffle(Deck deck) {
-        //TODO: return a shuffled copy of the deck
-        return null;
+        Random random = new Random(System.nanoTime());
+
+        // Fisher-Yates shuffle
+        List<Card> cards = Lists.newArrayList(deck);
+        for (int i = deck.size(); i > 1; i--) {
+            cards.set(i - 1, cards.set(random.nextInt(i), cards.get(i - 1)));
+        }
+        return Deck.of(cards);
     }
 }
